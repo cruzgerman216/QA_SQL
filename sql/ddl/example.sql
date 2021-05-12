0113c96ba77dbd331474e4ff59d5d891d3e32890
cruzgerman216 <cruzgerman216@yahoo.com>
2021-05-12 10:52:58 -0500

    gc: adjusted foreign keys and got rid of table_name

--- sql/ddl/example.sql
+++ sql/ddl/example.sql
@@ -22,14 +22,15 @@ CREATE TABLE orders (
 	order_id INT NOT NULL,
 	quantity INT NOT NULL, 
 	date_placed DATETIME NOT NULL,
+	fk_customer_id INT NOT NULL,
+	fk_product_id INT NOT NULL,
 	FOREIGN KEY (fk_customer_id) REFERENCES customers(customer_id),
+	FOREIGN KEY (fk_product_id) REFERENCES products(product_id),
 	PRIMARY KEY (order_id)
 );
 
-CREATE TABLE table_name (
-table_name INT NOT NULL,
-fk_customer_id INT NOT NULL,
-  FOREIGN KEY (fk_customer_id) REFERENCES customers(customer_id)
-);
 show tables;
-desc table_name;
+-- DROP TABLE customers;
+-- DROP TABLE products;
+-- DROP TABLE orders;
+
